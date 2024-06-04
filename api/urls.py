9a15c9745e50e5fb76934fa9c6078adea9d89c64
dmytro.bakai <bakay.dmytro@gmail.com>
2024-06-05 00:58:43 +0300

    Minor issues fix

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,45 @@
 from django.urls import path
 
-from .views import Home, UserRegistrationView, UserLoginView, AccountListCreateView, AccountRetrieveDestroyView, \
-    FecebookedirectUrlView, FacebookOauth2Login, UserDataView, CreateInstagramPost, CaptionImage, GenerateDescriptionForImage
+from .views import (
+    Home,
+    UserRegistrationView,
+    UserLoginView,
+    AccountListCreateView,
+    AccountRetrieveDestroyView,
+    FecebookedirectUrlView,
+    FacebookOauth2Login,
+    UserDataView,
+    CreateInstagramPost,
+    CaptionImage,
+    GenerateDescriptionForImage,
+    GenerateImageBasedOnDescription,
+)
 
 urlpatterns = [
-    path('', Home.as_view(), name='default-view'),
-    path('/register', UserRegistrationView.as_view(),name='user-registration'),
-    path('/login', UserLoginView.as_view(),name='user-login'),
-    path('/accounts', AccountListCreateView.as_view(), name='account-list-create'),
-    path('/accounts/<int:pk>', AccountRetrieveDestroyView.as_view(), name='account-retrieve-destroy'),
-    path('/login/oauth2/facebook', FacebookOauth2Login.as_view(), name='login-oauth'),
-    path('/login/oauth2/facebook/redirect', FecebookedirectUrlView.as_view(), name='redirect-oauth-url'),
-    path('/user/<int:id>', UserDataView.as_view(), name='user-data-view'),
-    path('/posts/create', CreateInstagramPost.as_view(), name='create-instagram-post' ),
-    path('/posts/caption', CaptionImage.as_view(), name='caption-image'),
-    path('/posts/description', GenerateDescriptionForImage.as_view(), name="post-description")
+    path("", Home.as_view(), name="default-view"),
+    path("/register", UserRegistrationView.as_view(), name="user-registration"),
+    path("/login", UserLoginView.as_view(), name="user-login"),
+    path("/accounts", AccountListCreateView.as_view(), name="account-list-create"),
+    path(
+        "/accounts/<int:pk>",
+        AccountRetrieveDestroyView.as_view(),
+        name="account-retrieve-destroy",
+    ),
+    path("/login/oauth2/facebook", FacebookOauth2Login.as_view(), name="login-oauth"),
+    path(
+        "/login/oauth2/facebook/redirect",
+        FecebookedirectUrlView.as_view(),
+        name="redirect-oauth-url",
+    ),
+    path("/user/<int:id>", UserDataView.as_view(), name="user-data-view"),
+    path("/posts/create", CreateInstagramPost.as_view(), name="create-instagram-post"),
+    path("/posts/caption", CaptionImage.as_view(), name="caption-image"),
+    path(
+        "/posts/description",
+        GenerateDescriptionForImage.as_view(),
+        name="post-description",
+    ),
+    path(
+        "/posts/image", GenerateImageBasedOnDescription.as_view(), name="generate-image"
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ import os
 import jwt
 import requests
 import vertexai
+from openai import OpenAI
 from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
@@ -27,14 +28,19 @@ from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
 load_dotenv()
 
-client_id = os.getenv('FACEBOOK_APP_ID')
-client_secret = os.getenv('FACEBOOK_APP_SECRET')
-redirect_uri = os.getenv('FACEBOOK_APP_REDIRECT_URL')
-config_id = os.getenv('FACEBOOK_APP_CONFIGURATION_ID')
-oauth2_base_url = os.getenv('FACEBOOK_LOG_IN_API_BASE_URL')
-graph_base_url = os.getenv('FACEBOOK_GRAPH_API_BASE_URL')
-gcp_project_id = os.getenv('GOOGLE_PROJECT_ID')
-vertexai.init(project=gcp_project_id, location='europe-west2')
+client_id = os.getenv("FACEBOOK_APP_ID")
+client_secret = os.getenv("FACEBOOK_APP_SECRET")
+redirect_uri = os.getenv("FACEBOOK_APP_REDIRECT_URL")
+config_id = os.getenv("FACEBOOK_APP_CONFIGURATION_ID")
+oauth2_base_url = os.getenv("FACEBOOK_LOG_IN_API_BASE_URL")
+graph_base_url = os.getenv("FACEBOOK_GRAPH_API_BASE_URL")
+gcp_project_id = os.getenv("GOOGLE_PROJECT_ID")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=openai_api_key)
+
+
+vertexai.init(project=gcp_project_id, location="europe-west2")
 
 imageModel = ImageTextModel.from_pretrained("imagetext@001")
 textModel = TextGenerationModel.from_pretrained("text-bison@002")
@@ -43,7 +49,7 @@ textModel = TextGenerationModel.from_pretrained("text-bison@002")
 class Home(APIView):
     def get(self, request):
         print(request.user.email)
-        content = {'message': 'Hello, World!'}
+        content = {"message": "Hello, World!"}
         return Response(content)
 
 
@@ -59,7 +65,10 @@ class UserRegistrationView(APIView):
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
 
-            return Response({'token': access_token, "user_id": user.id}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"token": access_token, "user_id": user.id},
+                status=status.HTTP_201_CREATED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -70,15 +79,18 @@ class UserLoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
-            email = serializer.validated_data.get('email')
-            password = serializer.validated_data.get('password')
+            email = serializer.validated_data.get("email")
+            password = serializer.validated_data.get("password")
 
             user = authenticate(request, username=email, password=password)
 
             if user is not None:
                 refresh = RefreshToken.for_user(user)
                 access_token = str(refresh.access_token)
-                return Response({'token': access_token, "user_id": user.id}, status=status.HTTP_201_CREATED)
+                return Response(
+                    {"token": access_token, "user_id": user.id},
+                    status=status.HTTP_201_CREATED,
+                )
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
@@ -89,9 +101,9 @@ class FacebookOauth2Login(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        token = request.GET.get('userAccessToken')
-        print("USER TOKEN FROM UI: " + token)
+        token = request.GET.get("userAccessToken")
         format_base_url = f"{oauth2_base_url}/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&config_id={config_id}&state={token}"
+        print(f"Formated base url: \n{format_base_url}")
         return HttpResponseRedirect(format_base_url)
 
 
@@ -109,7 +121,7 @@ class AccountListByUserView(generics.ListAPIView):
     serializer_class = AccountSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['user_id']
+        user_id = self.kwargs["user_id"]
         return Account.objects.filter(user_id=user_id)
 
 
@@ -117,16 +129,17 @@ class FecebookedirectUrlView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        oauth_code = request.query_params.get('code', None)
-        token = request.query_params.get('state', None)
+        oauth_code = request.query_params.get("code", None)
+        token = request.query_params.get("state", None)
         user_id = None
         try:
-            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-            user_id = decoded_token.get('user_id')
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            user_id = decoded_token.get("user_id")
         except Exception as e:
             print("Error parse user token", str(e))
 
-        print('USER ID: ' + str(user_id))
+        print("USER ID: " + str(user_id))
+        print("OAUTH CODE: " + str(oauth_code))
         if oauth_code and user_id:
             try:
                 access_token = getAccessToken(oauth_code)
@@ -137,7 +150,7 @@ class FecebookedirectUrlView(APIView):
                 user.instagram_user_id = ig_id
                 user.save()
             except Exception as e:
-                print('Error to get user data', str(e))
+                print("Error to get user data", str(e))
         else:
             print("Code parameter is missing")
         if user_id:
@@ -150,14 +163,17 @@ def getAccessToken(code):
     print("Code:", code)
     access_token_url = getAccessTokenUrl(code)
     print("Access token url: " + access_token_url)
-    response = requests.get(access_token_url)
-    if response.status_code == 200:
-        json_data = response.json()
-        access_token = json_data.get('access_token')
-        print("Access Token: " + access_token)
-        return access_token
-    else:
-        print('Error to get Access Token')
+    try:
+        response = requests.get(access_token_url)
+        if response.status_code == 200:
+            json_data = response.json()
+            access_token = json_data.get("access_token")
+            print("Access Token: " + access_token)
+            return access_token
+        else:
+            print("Error to get Access Token")
+    except Exception as e:
+        print("Error to get Access Token", e)
 
 
 def getAccessTokenUrl(code):
@@ -172,11 +188,11 @@ def getInstagramUserId(access_token):
     ig_business_account_response = requests.get(ig_business_account_url, None)
     if ig_business_account_response.status_code == 200:
         json_body = ig_business_account_response.json()
-        ig_user_id = json_body.get('instagram_business_account', {}).get('id')
-        print('Instagram business account id: ' + ig_user_id)
+        ig_user_id = json_body.get("instagram_business_account", {}).get("id")
+        print("Instagram business account id: " + ig_user_id)
         return ig_user_id
     else:
-        print('Error to get instagram business data')
+        print("Error to get instagram business data")
 
 
 def getFacebookPageId(access_token):
@@ -185,28 +201,33 @@ def getFacebookPageId(access_token):
     accounts_response = requests.get(get_accounts_url, None)
     if accounts_response.status_code == 200:
         json_data = accounts_response.json()
-        data = json_data.get('data', [])
+        data = json_data.get("data", [])
         if data:
-            page_id = data[0].get('id')
+            page_id = data[0].get("id")
             print("Page id: " + page_id)
             return page_id
         else:
             print("No data found in accounts list")
     else:
-        print('Error to get accounts data')
+        print("Error to get accounts data")
     return None
 
 
 class UserDataView(APIView):
     def get(self, request, id):
         user = get_object_or_404(User, id=id)
-        need_facebook_auth = user.instagram_user_id is None or user.instagram_user_id == "" or user.access_token is None or user.access_token == ""
+        need_facebook_auth = (
+            user.instagram_user_id is None
+            or user.instagram_user_id == ""
+            or user.access_token is None
+            or user.access_token == ""
+        )
         user_data = {
             "id": user.id,
             "email": user.email,
             "first_name": user.first_name,
             "last_name": user.last_name,
-            "needFacebookAuth": need_facebook_auth
+            "needFacebookAuth": need_facebook_auth,
         }
 
         return Response(user_data, status=status.HTTP_200_OK)
@@ -214,76 +235,67 @@ class UserDataView(APIView):
 
 class CreateInstagramPost(APIView):
     def post(self, request):
-        print('gettin post reqest')
         data = request.data
         user = request.user
         instagram_media_response = createInstagramMediaContainer(data, user)
         if instagram_media_response.status_code == 200:
             json = instagram_media_response.json()
-            print("JSON")
-            print(json)
-            media_container_id = json.get('id')
+            media_container_id = json.get("id")
             publishMediaContainer(media_container_id, user)
         else:
-            print("Error to create instagram media record")
-        return Response("SUCCESS")
+            return Response(
+                "Error to create instagram media record",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        return Response("Instagram post successfully created")
 
 
 def createInstagramMediaContainer(request_data, user):
     instagram_media_url = f"{graph_base_url}/{user.instagram_user_id}/media"
     data = {
-        "image_url": request_data.get('image_url'),
-        "caption": request_data.get('description'),
-        "access_token": user.access_token
+        "image_url": request_data.get("image_url"),
+        "caption": request_data.get("description"),
+        "access_token": user.access_token,
     }
     response = requests.post(instagram_media_url, data)
-    print("Response from media:")
-    print(response)
+    print(f"Response from media: {response}")
     return response
 
 
 def publishMediaContainer(container_id, user):
     publish_media_url = f"{graph_base_url}/{user.instagram_user_id}/media_publish"
-    print(publish_media_url)
-    data = {
-        "creation_id": container_id,
-        "access_token": user.access_token
-    }
-    print('Publishing media container')
-    print(data)
+    print(f"Publish media url: {publish_media_url}")
+    data = {"creation_id": container_id, "access_token": user.access_token}
+    print(f"Publishing media container with data: {data}")
     response = requests.post(publish_media_url, data)
-    print('Response from publishing container:')
-    print(response)
+    print(f"Response from publishing container:{response}")
     return response
-
-
 
 
 class CaptionImage(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        image_url = request.GET.get('imageUrl')
+        image_url = request.GET.get("imageUrl")
+        print(f"ImageURL: {image_url}")
         image_path = downloadImage(image_url)
         print("image path: " + image_path)
         source_img = Image.load_from_file(image_path)
-        captions = ''
+        captions = ""
         try:
             captions = imageModel.get_captions(
-                image=source_img,
-                language='en',
-                number_of_results=1)
+                image=source_img, language="en", number_of_results=1
+            )
         except Exception as e:
             print("Error occurs(", str(e))
-        print("Captions: " + str(captions))
-        data = {
-            'captions': captions
-        }
+        print("Caption: " + str(captions))
+        data = {"caption": captions}
         return Response(data, status=status.HTTP_200_OK)
 
 
 def downloadImage(image_url):
-    upload_folder = os.path.join(settings.BASE_DIR, 'upload')
+    upload_folder = os.path.join(settings.BASE_DIR, "upload")
     if not os.path.exists(upload_folder):
         os.makedirs(upload_folder)
     try:
@@ -291,12 +303,12 @@ def downloadImage(image_url):
         response.raise_for_status()
     except requests.RequestException as e:
         print({"error": str(e)}, status=400)
-    image_name = image_url.split('/')[-1]
+    image_name = image_url.split("/")[-1]
     image_path = os.path.join(upload_folder, image_name)
     if os.path.exists(image_path):
         return image_path
 
-    with open(image_path, 'wb') as f:
+    with open(image_path, "wb") as f:
         for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
     return image_path
@@ -305,26 +317,70 @@ def downloadImage(image_url):
 class GenerateDescriptionForImage(APIView):
     def post(self, request):
         data = request.data
-        prompt = 'Generate description for instagram post. Using next data:'
-        if data.get('description'):
-            prompt = prompt + 'details description: '+ data.get('description')+ '\n' 
-        if data.get('caption'):
-            prompt = prompt + 'image caption: '+ data.get('caption')
+        prompt = (
+            "Generate a description for an Instagram post using the following data:\n"
+        )
 
+        theme = data.get("theme")
+        caption = data.get("caption")
+
+        if theme:
+            prompt += f"Post theme: {theme}\n"
+        if caption:
+            prompt += f"Image caption: {' '.join(caption)}"
         response = generateDescription(prompt)
-        data = {
-            "description": response
-        }
+        data = {"description": response}
 
         return Response(data, status=status.HTTP_200_OK)
 
+
 def generateDescription(prompt):
-        parameters = {
-            "temperature": 0.5,
-            "max_output_tokens": 256, 
-            "top_p": 0.8,  
-            "top_k": 40,
-        }
-        response = textModel.predict(prompt,**parameters,)
-        print(f"Response from Model: {response.text}")
-        return response.text
+    parameters = {
+        "temperature": 1,
+        "max_output_tokens": 256,
+        "top_p": 0.8,
+        "top_k": 40,
+    }
+    response = textModel.predict(
+        prompt,
+        **parameters,
+    )
+    print(f"Response from Model: {response.text}")
+    return response.text
+
+
+def generate_image(description):
+    try:
+        prompt = f"Generate image for instagram post based on image description: {description}"
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        image_url = response.data[0].url
+        return image_url
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+class GenerateImageBasedOnDescription(APIView):
+    def post(self, request):
+        data = request.data
+        try:
+            image_url = generate_image(data["description"])
+            if not image_url:
+                return Response(
+                    "Error to generate image",
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+            print(f"Image URL: {image_url}")
+            data = {"imageUrl": image_url}
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return Response(
+                "Error to generate image", status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
